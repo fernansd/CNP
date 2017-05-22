ade3d7f19fa26f5d8d0b691b7e9a1f0420c612aa
vep1940 <victormanu_96@hotmail.com>
2017-05-22 19:52:57 +0200

    SolGenerator.cpp

---
+++ CNPSolGenerator.cpp
@@ -0,0 +1,25 @@
+/*
+ * CNPSolGenerator.cpp
+ *
+ * Fichero que define los métodos de la clase CNPSolGenerator. 
+ * Forma parte del código esqueleto para el problema de las múltiples mochilas cuadráticas, 
+ * ofrecido para las prácticas de la asignatura Metaheurísticas del 
+ * Grado de Ingeniería Informática de la Universidad de Córdoba
+ *
+ * @author Undead Corporation (vep1940)
+ */
+
+#include "CNPSolGenerator.h"
+#include "CNPInstance.h"
+#include "CNPSolution.h"
+#include <stdlib.h>
+
+void CNPSolGenerator::genRandomSol(CNPInstance &instance, CNPSolution &solution){
+
+	unsigned numNodes=instance.getNumNodes()
+	unsigned numMaxCrit=instance.getNumCritMax();
+
+	while(solution.getNumCrit!=numMaxCrit){
+		solution.setNode(rand()%numNodes,true);
+	}
+}
